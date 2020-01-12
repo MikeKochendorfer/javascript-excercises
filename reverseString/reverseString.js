99a3b26fd231c1a4fafe5f4bd9ea5dfab1401720
MikeKochendorfer <mike.kochendorfer@gmail.com>
2020-01-11 22:41:22 -0700

    Solved.

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	stringAsArray = string.split('')
+	arrayReversed = stringAsArray.reverse()
+	stringReversed = arrayReversed.join('')
+	return stringReversed
 }
 
 module.exports = reverseString
