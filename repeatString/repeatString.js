24194f728a520c115db4d18cd48224fbe0f912bc
MikeKochendorfer <mike.kochendorfer@gmail.com>
2020-01-11 21:48:57 -0700

    Solved.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+	if (number >= 0) {
+		stringRepeat = ''
+		for (i = 0; i < number; i++){
+		stringRepeat = stringRepeat.concat(string)
+	}
+		return stringRepeat
+	} else {
+		return 'ERROR'
+	}
 }
 
 module.exports = repeatString
