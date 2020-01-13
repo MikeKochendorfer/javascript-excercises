fb9ad73a2b907bea42d32d9e7768674fd159fac2
MikeKochendorfer <mike.kochendorfer@gmail.com>
2020-01-13 12:15:42 -0700

    Solved.

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+	if (int1 < 0 || int2 < 0) return 'ERROR';
+	if (typeof int1 !== 'number' || typeof int2 !== 'number') return 'ERROR';
 
+	let argsSorted = [int1, int2].sort()
+	let sum = []
+	for (let i = argsSorted[0]; i < argsSorted[argsSorted.length-1] + 1; i++) {
+		sum.push(i)
+	}
+	const reducer = (accumulator, currentValue) => accumulator + currentValue;
+	return sum.reduce(reducer)
 }
 
 module.exports = sumAll
