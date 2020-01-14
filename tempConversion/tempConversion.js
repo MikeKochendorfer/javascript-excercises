df172d062f240fe41782355af5ddcec0a83a0871
MikeKochendorfer <mike.kochendorfer@gmail.com>
2020-01-14 10:37:45 -0700

    Solved

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahrenheit) {
+	celciusTemp = (tempFahrenheit - 32) * (5/9)
+	return Math.round(celciusTemp * 10) / 10
 }
 
-const ctof = function() {
-
+const ctof = function(tempCelcius) {
+	fahrenheitTemp = tempCelcius * (9/5) + 32
+	return Math.round(fahrenheitTemp * 10) / 10
 }
 
 module.exports = {
